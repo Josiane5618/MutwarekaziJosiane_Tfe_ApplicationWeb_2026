fb047a7ebacea2fceddb0265ad0ecf266d214b2b
Josiane <kajosine@gmail.com>
2026-04-21 11:09:55 +0200

    Sprint 5.4__ Créer le modèle LogAcces Créer la table en base .Enregistrer les logs dans /access/verify Tester le fonctionnement

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI
 from app.routers.auth import router as auth_router
 from app.routers.admin import router as admin_router
 from app.routers.access import router as access_router
+from app.models import log_acces
+
 
 app = FastAPI(
     title="Système de gestion d’accès et de réservation"
