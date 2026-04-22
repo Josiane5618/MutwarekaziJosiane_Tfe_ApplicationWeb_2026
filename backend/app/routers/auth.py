95ddb6ab8d44d16555129a2e04ac66558a6ec03a
Josiane <mutwarekazij@gmail.com>
2026-04-22 15:36:59 +0200

    Créer le frontend,Installer les dépendances,Lancer le frontend

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, UploadFile, File
 from sqlalchemy.orm import Session
 
 from app.database import SessionLocal
@@ -7,7 +7,6 @@ from app.schemas.user import UserRegister, UserLogin
 from app.security.dependencies import get_current_admin
 from app.security.password import hash_password, verify_password
 from app.security.jwt import create_access_token
-
 
 router = APIRouter(
     prefix="/auth",
@@ -97,3 +96,47 @@ def login_user(
         "access_token": token,
         "token_type": "bearer"
     }
+@router.post("/register")
+async def register(
+    email: str,
+    password: str,
+    file: UploadFile = File(...),
+    db: Session = Depends(get_db)
+):
+    #  Vérifier email unique
+    existing = db.query(Utilisateur).filter(Utilisateur.email == email).first()
+    if existing:
+        raise HTTPException(status_code=400, detail="Email déjà utilisé")
+
+    # Lecture image faciale
+    image_bytes = await file.read()
+
+    # Extraction encodage facial
+    encoding = extract_face_encoding(image_bytes)
+    if encoding is None:
+        raise HTTPException(
+            status_code=400,
+            detail="Aucun visage détecté lors de l'inscription"
+        )
+
+    #  Création utilisateur (non actif)
+    user = Utilisateur(
+        email=email,
+        mot_de_passe=hash_password(password),
+        statut="EN_ATTENTE",
+        role="UTILISATEUR"
+    )
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+    #Enregistrement des données faciales
+    faciale = DonneeFaciale(
+        utilisateur_id=user.id,
+        image=image_bytes,
+        encodage=encoding.tobytes()
+    )
+    db.add(faciale)
+    db.commit()
+
+    return {"message": "Inscription soumise. En attente de validation admin."}
