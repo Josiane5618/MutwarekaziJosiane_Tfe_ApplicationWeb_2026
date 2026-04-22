7e22321a3d49e6d3217091e32b75aea2f04d8fde
Josiane <kajosine@gmail.com>
2026-04-22 10:37:14 +0200

    Visualisation directe de l’image faciale brute (admin) Envoi réel d’email après acceptation/refus  Route admin globale de consultation (réservations + logs)

---
+++ backend/app/utils/email_service.py
@@ -0,0 +1,6 @@
+def send_email(to_email: str, subject: str, content: str):
+    print("EMAIL SIMULÉ")
+    print("À :", to_email)
+    print("Sujet :", subject)
+    print("Message :", content)
+    print("-" * 40)
